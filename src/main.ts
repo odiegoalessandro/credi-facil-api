b5fc3f49fd40c45e9e3e0c56968da0fc7ae86ba5
Diego alessandro da cruz martins <diegoalessandrodacruzmartins@gmail.com>
2024-07-12 21:46:21 -0300

    feat: criando pipes e validators

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { HttpExceptionFilter } from './filter/http.exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
+
+  app.useGlobalFilters(new HttpExceptionFilter());
+
   await app.listen(3000);
 }
 bootstrap();
